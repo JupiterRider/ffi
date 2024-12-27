d7fb1387604361503965eb57f174c246ebcf7d0c
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2024-12-27 12:45:30 +0100

    new method Lib.PrepVar added

--- lib.go
+++ lib.go
@@ -20,3 +20,16 @@ func (l Lib) Prep(name string, ret *Type, args ...*Type) (f Fun, err error) {
 
 	return
 }
+
+func (l Lib) PrepVar(name string, nFixedArgs int, ret *Type, args ...*Type) (f Fun, err error) {
+	if f.Addr, err = l.Get(name); err != nil {
+		return
+	}
+
+	f.Cif = new(Cif)
+	if status := PrepCifVar(f.Cif, DefaultAbi, uint32(nFixedArgs), uint32(len(args)), ret, args...); status != OK {
+		return f, fmt.Errorf("%s: error preparing function: %s", name, status)
+	}
+
+	return
+}
