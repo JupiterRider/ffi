17e5a2a6a36481149b9b8c386b481f01708e3520
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2024-12-27 20:01:01 +0100

    update simple/cos example: use Lib and Func types

---
+++ examples/simple/cos/main.go
@@ -0,0 +1,43 @@
+//go:build (freebsd || linux || darwin || windows) && (amd64 || arm64)
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/jupiterrider/ffi"
+)
+
+func main() {
+	var filename string
+	switch runtime.GOOS {
+	case "linux":
+		filename = "libm.so.6"
+	case "freebsd":
+		filename = "libm.so.5"
+	case "darwin":
+		filename = "libm.dylib"
+	case "windows":
+		filename = "ntdll.dll"
+	}
+
+	// open the shared library
+	libm, err := ffi.Load(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	// get the function's address and describe its signature
+	cos, err := libm.Prep("cos", &ffi.TypeDouble, &ffi.TypeDouble)
+	if err != nil {
+		panic(err)
+	}
+
+	// call the function
+	cosine, x := 0.0, 1.0
+	cos.Call(&cosine, &x)
+
+	// prints 0.5403023058681398
+	fmt.Println(cosine)
+}
