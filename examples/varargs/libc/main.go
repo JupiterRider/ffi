5194fea55a92e24545651fcd68bb07a1cd97d5de
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2024-12-27 20:21:34 +0100

    update varargs/libc example: use Lib and Func types and add Windows + macOS support

--- examples/varargs/libc/main.go
+++ examples/varargs/libc/main.go
@@ -1,4 +1,4 @@
-//go:build (freebsd || linux) && (amd64 || arm64)
+//go:build (freebsd || linux || darwin || windows) && (amd64 || arm64)
 
 package main
 
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"math"
 	"runtime"
-	"unsafe"
 
-	"github.com/ebitengine/purego"
 	"github.com/jupiterrider/ffi"
 )
 
@@ -19,27 +17,26 @@ func main() {
 		filename = "libc.so.6"
 	case "freebsd":
 		filename = "libc.so.7"
+	case "windows":
+		filename = "msvcrt.dll"
+	case "darwin":
+		filename = "libc.dylib"
 	}
 
-	libc, err := purego.Dlopen(filename, purego.RTLD_LAZY)
+	libc, err := ffi.Load(filename)
 	if err != nil {
 		panic(err)
 	}
 
-	printf, err := purego.Dlsym(libc, "printf")
+	// printf is a variadic function, with one fixed argument
+	printf, err := libc.PrepVar("printf", 1, &ffi.TypeSint32, &ffi.TypePointer, &ffi.TypeDouble)
 	if err != nil {
 		panic(err)
-	}
-
-	var cif ffi.Cif
-	status := ffi.PrepCifVar(&cif, ffi.DefaultAbi, 1, 2, &ffi.TypeSint32, &ffi.TypePointer, &ffi.TypeDouble)
-	if status != ffi.OK {
-		panic(status)
 	}
 
 	text := &[]byte("Pi is %f\n\x00")[0] // C requires a null-terminated string
 	pi := math.Pi
 	var nCharsPrinted ffi.Arg
-	ffi.Call(&cif, printf, unsafe.Pointer(&nCharsPrinted), unsafe.Pointer(&text), unsafe.Pointer(&pi))
+	printf.Call(&nCharsPrinted, &text, &pi)
 	fmt.Printf("%d characters printed\n", int32(nCharsPrinted))
 }
